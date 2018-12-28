ed3354aeecd2b5cba6743bf4ba1cda09ceebc855
SoPlump <sophie.raudrant@gmail.com>
2018-12-28 18:49:56 +0100

    Modification de la fonction Charger
    Pour l'instant affiche une ligne du fichier csv selon une condition

--- src/Interface.cpp
+++ src/Interface.cpp
@@ -13,6 +13,10 @@ e-mail               : guilhem.cerba@insa-lyon.fr, sophie.raudrant@insa-lyon.fr
 //-------------------------------------------------------- Include systeme
 #include <iostream>
 #include <cstring>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
 using namespace std;
 
 //------------------------------------------------------ Include personnel
@@ -27,7 +31,7 @@ using namespace std;
 // Algorithme :
 //
 //{
-//} //----- Fin de Methode
+//} //----- fin de Methode
 
 
 //-------------------------------------------- Constructeurs - destructeur
@@ -41,14 +45,14 @@ void Interface::Menu()
 //4. Afficher tout le Interface
 {
 	char lecture[100];
-	cout << "Bienvenue !" << endl;
-	cout << "****************************" << endl;
-	cout << "1. Ajouter un trajet simple" << endl;
-	cout << "2. Ajouter un trajet compose" << endl;
-	cout << "3. Chercher un Trajet Complexe" << endl;
-	cout << "4. Chercher un Trajet Simple" << endl;
-	cout << "5. Afficher tout le Interface" << endl;
-	cout << "****************************" << endl;
+	std::cout << "Bienvenue !" << endl;
+	std::cout << "****************************" << endl;
+	std::cout << "1. Ajouter un trajet simple" << endl;
+	std::cout << "2. Ajouter un trajet compose" << endl;
+	std::cout << "3. Chercher un Trajet Complexe" << endl;
+	std::cout << "4. Chercher un Trajet Simple" << endl;
+	std::cout << "5. Afficher tout le Interface" << endl;
+	std::cout << "****************************" << endl;
 
 	fscanf(stdin, "%99s", lecture);
 	while (strcmp(lecture, "bye") != 0)
@@ -69,10 +73,11 @@ void Interface::Menu()
 
 		fscanf(stdin, "%99s", lecture);
 	}
-}// ---- fin de Menu
+}// ---- file de Menu
 
 void Interface::Afficher() const
 {
+
 }// ---- fin de Afficher
 
 
@@ -83,14 +88,81 @@ void Interface::AjouterTrajet()
 void Interface::Rechercher() const
 {
 
-}
+}// ---- fin de Rechercher
 
 void Interface::Charger()
-{
+{    
+	// File pointer 
+    fstream file; 
+  
+    // Open an existing file 
+    file.open("test.csv", ios::in); 
+  
+    // Get the roll number 
+    // of which the data is required 
+    int rollnum, roll2, count = 0; 
+    cout << "Enter the roll number "
+         << "of the student to display details: "; 
+    cin >> rollnum; 
+  
+    // Read the Data from the file 
+    // as String Vector 
+    vector<string> row; 
+    string line, word, temp; 
+  
+    while (file >> temp) 
+    { 
+  
+        row.clear(); 
+  
+        // read an entire row and 
+        // store it in a string variable 'line' 
+        getline(file, line); 
+  
+        // used for breaking words 
+        stringstream s(line); 
+  
+        // read every column data of a row and 
+        // store it in a string variable, 'word' 
+        while (getline(s, word, ',')) 
+        { 
+  
+            // add all the column data 
+            // of a row to a vector 
+            row.push_back(word); 
+        } 
+  
+        // convert string to integer for comparison 
+        roll2 = stoi(row[0]); 
+  		
 
-}
+  		// TODO: Faire en sorte d'Afficher toutes les lignes
+        // Compare the roll number 
+        if (roll2 == rollnum) 
+        { 
+  
+            // Print the found data 
+            count = 1; 
+            cout << "Details of Roll " << row[0] << " : \n"; 
+            cout << "Name: " << row[1] << "\n"; 
+            cout << "Maths: " << row[2] << "\n"; 
+            cout << "Physics: " << row[3] << "\n"; 
+            cout << "Chemistry: " << row[4] << "\n"; 
+            cout << "Biology: " << row[5] << "\n"; 
+            break; 
+        } 
+    } 
+    if (count == 0) 
+        cout << "Record not found\n"; 
+
+}// ---- file de Charger
 
 void Interface::Sauvegarder()
+{
+
+}// ---- file de Sauvegarder
+
+Interface::Interface()
 {
 
 }
@@ -100,7 +172,7 @@ Interface::~Interface()
 #ifdef MAP
 	cout << "Appel au destructeur de <Interface>" << endl;
 #endif
-}// ---- fin de ~Interface
+}// ---- file de ~Interface
 
 //------------------------------------------------------------------ PRIVE
 
