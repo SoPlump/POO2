4afd397b7bedac4f0812c2c9fffbb22c7ba2b642
SoPlump <sophie.raudrant@gmail.com>
2019-02-09 11:24:31 +0100

    Update main.cpp
    Ajout de test

--- TP2/src/main.cpp
+++ TP2/src/main.cpp
@@ -4,28 +4,23 @@
 
 int main(int argn, char** argv)
 {
-	bool e = false, t = false;
-	int heure = 0;
-
 	Data* data = new Data();
-	FileManager* fileM = new FileManager();
+	//FileManager* fileM;
 
-	if ( e )
-	{
-		if ( t )
-		{
-			//Data( e, heure );
-		}
-		else
-		{
-			//Data( e );
-		}
-	}
-	else if ( t )
-	{
-		//Data( heure );
-	}
-	else
-	{
-	}
+	data->Ajouter("b", "a");
+	data->AddNode("b", "a");
+
+	data->Ajouter("c", "a");
+	data->AddNode("c", "a");
+	data->Ajouter("c", "a");
+	data->AddNode("c", "a");
+
+	data->Ajouter("b", "c");
+	data->AddNode("b", "c");
+	data->Ajouter("b", "c");
+	data->AddNode("b", "c");
+
+	cout << *data;
+
+	delete data;
 }
