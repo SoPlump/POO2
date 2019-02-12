cc3a80ebd6675bc0b611a2b3ab754ab068fbd9b0
Ahmed Abdel Rahman <ahmed.abdel_rahman@hotmail.com>
2019-02-12 19:06:46 +0100

    Revert "Verif graphviz existant"
    This reverts commit c721e3f06359200702d4158652018e121326f70d.

--- TP2/src/Data.h
+++ TP2/src/Data.h
@@ -68,7 +68,7 @@ public:
 
     bool Traiter ();
 
-    void AfficherTopTen ();
+    void AfficherTopTen ( );
 
 //-------------------------------------------- Constructeurs - destructeur
     Data (Options opt);
@@ -99,7 +99,7 @@ protected:
     // Contrat :
     
 ///----------------------------------------------------- Attributs protégés
-    Options m_choix;
+    Options choix;
     std::map<std::string, Noeud*> m_docInfo;
     std::map<std::string, uint> m_nodes;
 };
