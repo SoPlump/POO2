7f30659ea88d783edfdc1e66cb97e56233da98ee
SoPlump <sophie.raudrant@gmail.com>
2019-02-12 19:40:49 +0100

    Update Data.cpp
    Ajout commentaires

--- TP2/src/Data.cpp
+++ TP2/src/Data.cpp
@@ -31,18 +31,23 @@ using namespace std;
 void Data::Ajouter ( string source, string cible )
 {
 	map<string, Noeud*>::iterator it = m_docInfo.find(cible);
-	if ( it == m_docInfo.end())
+
+	// Verifie si le site cible existe deja dans la map qui contient tous les sites cibles
+	if ( it == m_docInfo.end()) // Site non trouvee
 	{
-		// not found
+		// On cree un nouveau noeud qui contiendra tous les sites sources qui
+		// ont referes le site cible avec le nombre d'occurence (par site source et au total)
 		Noeud* noeud = new Noeud(source);
+
 		m_docInfo.insert(make_pair(cible, noeud));
 	}
-	else
+	else // Site trouve
 	{
-		// found
+		// On ajoute le site source au Noeud correspondant au site cible
 		it->second->Ajouter(source);
 	}
 
+	// On ajoute une node
 	AddNode(source, cible);
 }
 
@@ -50,11 +55,14 @@ void Data::AddNode ( string source, string cible )
 {
 	static uint index = 0;
 
+	// On verifie si le site cible existe deja dans la map qui contient toutes les nodes et leur index
 	map<string, uint>::iterator it = m_nodes.find(cible);
 	if ( it == m_nodes.end())
 	{
 		m_nodes.insert(make_pair(cible, index++));
 	}
+
+	// On verifie si le site source existe deja dans la map qui contient toutes les nodes et leur index
 	it = m_nodes.find(source);
 	if ( it == m_nodes.end())
 	{
@@ -67,24 +75,25 @@ bool Data::GenerateGraph ( const string & fileName )
 {
 
 	ofstream fileOut(fileName.c_str(), ios::trunc);
-	// Vérification de l'ouverture du fichier en écriture
+
+	// Verification de l'ouverture du fichier
 	if(!fileOut)
 	{
 		return false;
 	}
 
-    // Ouverture de la description du graphe
 	fileOut << "digraph {" << endl;
 
-	// Déclaration de l'ensemble des sites
-	// On parcours la table nodes contenant l'ensemble des sites
+	// On parcourt la map qui contient tous les sites avec un numero associe
 	for(auto it=m_nodes.begin(); it!=m_nodes.end(); it++)
 	{
 		fileOut << "\tnode" << it->second << " [label=\"" <<  it->first << "\"];" << endl;
 	}
 
+	// On parcourt la map qui contient tous les sites cible
 	for(auto itCible = m_docInfo.begin(); itCible != m_docInfo.end(); ++itCible)
 	{
+		// On recupere pour chacun des sites cibles la map des sites Sources qui le refere, avec le nombre d'occurence
 		map <std::string, uint> mapSource = itCible->second->GetMapSources();
 		for ( auto itSource = mapSource.begin() ; itSource != mapSource.end() ; ++itSource )
 		{
@@ -93,87 +102,61 @@ bool Data::GenerateGraph ( const string & fileName )
 		}
 	}
 
-	// Ecriture des arcs entre les sites
-	// On parcours la table edges contenant l'ensemble des arcs
-
-	/*map<string, uint> mapSource;
-	map<string, uint>::iterator itSource;
-
-	map<string, Noeud*>::iterator itCible;
-	map<string, uint>::iterator itNode;
-	for ( itCible = m_docInfo.begin(); itCible != m_docInfo.end(); itCible++ )
-	{
-		itNode = m_nodes.find(itCible->first);
-		mapSource = itCible->second->GetMapSources();
-		//fileOut << "\tnode" << itNode->second << " -> node" << pos->first.second;
-	}*/
-
-	/*for(auto pos=edges.begin(); pos!=edges.end(); pos++)
-	{
-		fileOut << "\tnode" << pos->first.first << " -> node" << pos->first.second;
-		fileOut << " [label=\"" << pos->second << "\"];" << endl;
-	}*/
-
-    // Fermeture de la description du graphe
 	fileOut << "}" << endl;
 
 	// Fermeture du flux d'écriture
 	fileOut.close();
+
 	return true;
 }
 
 bool Data::Traiter ()
 {
+	// On ouvre un FileManager
 	FileManager fileM(choix.logName);
 
 	while(!fileM.eof())
 	{
-		bool keep = true;
+		bool keep = true; // Nous permet de savoir si on garde la ligne
 		string line;
+
+		// Recupere chaque ligne du fichier log
 		getline(fileM,line);
 		stringstream s(line);
+
+		// Decoupe chaque ligne en un vecteur de mots
 		vector <string> v = fileM.Decouper(s);
 
-		/*for (int i = 0; i < v.size(); ++i)
+		if (!v.empty())
 		{
-			cout << v[i] << endl;
-		}*/
-
-		// Traitement type d'entrées (-e)
-		if (v.empty())
-		{
-			keep = false;
-		}
-		else
-		{
+			// Traitement type d'entrées (-e)
 			if(choix.eOption == 1)
 			{
 				if((v[6].find(".css",0)!= string::npos) || (v[6].find(".js",0)!= string::npos) || (v[6].find(".png",0)!= string::npos) || (v[6].find(".bmp",0)!= string::npos) || (v[6].find(".jpg",0)!= string::npos) || (v[6].find(".jpeg",0)!= string::npos) || (v[6].find(".gif",0)!= string::npos) || (v[6].find(".ico",0)!= string::npos))
 				{
+					// La ligne correspond à un site qui ne correspond pas a ce qui est desire
 					keep = false;
 				}
 			}
 			// Traitement intervalle temporelle (-t)
 			if(choix.tOption == 1)
 			{
-				cout << "wtf" <<endl;
 				size_t pos = v[3].find(':',0);
 				uint t = stoul(v[3].substr(pos+1, pos+2));
 
 				if(!(t==choix.hour))
 				{
+					// La ligne correspond à un site qui ne correspond pas a ce qui est desire
 					keep = false;
 				}
 			}
 
-			// Prise en compte dans le résultat
+			// Ajout de la cible et de la source dans la map qui contient tous les sites cibles
 			if(keep == true)
 			{
 				Ajouter(v[10], v[6]);
 			}
-
 		}
-
 	}
 
 	// Traitement création graphe (-g)
@@ -182,7 +165,7 @@ bool Data::Traiter ()
 		GenerateGraph(choix.graphName);
 	}
 
-	//AfficherTopTen();
+	AfficherTopTen();
 
 	return true;
 }
